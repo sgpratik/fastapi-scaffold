990d42322e44532fdce78db3e234cb927fc255b4
Kumar Pratik <kpratik@greenstreet.com>
2023-05-29 22:35:08 +0530

    added git online push

--- {{cookiecutter.project_name}}/setup_project.sh
+++ {{cookiecutter.project_name}}/setup_project.sh
@@ -7,3 +7,8 @@ echo "Project dependencies installed."
 echo "Initializing git repositiry for {{cookiecutter.project_name}} locally."
 git init
 echo "git repository for {{cookiecutter.project_name}} has been created locally please add an origin of your empty repository to push this project up online for version control."
+git remote add origin {{cookiecutter.repository_remote}}
+git add .
+git commit -m "Project Setup"
+git branch -M main
+git push -u origin main
